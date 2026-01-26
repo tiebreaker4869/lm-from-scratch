fix: fix gradient clipping

assignment1-basics/cs336_basics/training_utils.py
@@ -38,7 +38,7 @@ def learning_rate_schedule(t: int, alpha_max: float, alpha_min: float, T_w: int,
 
 def gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float, eps: float = 1e-6):
     params_with_grad = [param for param in parameters if param.grad is not None]
-    grad = torch.concat([param.grad for param in params_with_grad])
+    grad = torch.concat([param.grad.flatten() for param in params_with_grad])
     norm = torch.linalg.norm(grad, ord=2)
     if norm > max_l2_norm:
         for param in params_with_grad: