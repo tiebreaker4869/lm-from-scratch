7ec4fc382d0650e1606ef2c02b02316df8f04f96
Lifan Sun <lifansun1412@gmail.com>
2026-01-28 22:05:19 -0800

    feat: add no positional embedding ablation

--- assignment1-basics/cs336_basics/models.py
+++ assignment1-basics/cs336_basics/models.py
@@ -173,7 +173,7 @@ class MultiHeadSelfAttention(nn.Module):
         out = rearrange(out, 'bs ... num_heads seq_len d_h -> bs ... seq_len (num_heads d_h)')
         out = self.w_out(out)
         return out
-    
+
 class TransformerBlock(nn.Module):
     def __init__(self, d_model: int, num_heads: int, d_ff: int, theta: float | None = None, max_seq_len: int | None = None, device: torch.device | None = None, dtype: torch.dtype | None = None):
         super(TransformerBlock, self).__init__()
@@ -276,12 +276,29 @@ class MiniLMPostLN(nn.Module):
         out = self.final_norm(out)
         out = self.final_proj(out)
         return out
+
+class MiniLMNoPE(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, num_heads: int, theta: float, vocab_size: int, context_length: int, num_layers: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
+        super(MiniLMNoPE, self).__init__()
+        self.embeddings = Embedding(num_embeddings=vocab_size, embedding_dim=d_model, device=device, dtype=dtype)
+        self.blocks = nn.ModuleList([TransformerBlock(d_model, num_heads, d_ff, theta=None, max_seq_len=context_length, device=device, dtype=dtype) for _ in range(num_layers)]) # pass theta = None to disable positional encoding
+        self.final_norm = RMSNorm(d_model, device=device, dtype=dtype)
+        self.final_proj = Linear(d_model, vocab_size, device, dtype)
+        self.device = device
+    def forward(self, x: Int[Tensor, '... seq_len']) -> Float[Tensor, '... seq_len vocab_size']:
+        out = self.embeddings(x)
+        for block in self.blocks:
+            out = block(out)
+        out = self.final_norm(out)
+        out = self.final_proj(out)
+        return out
     
 def get_model(model: str):
     mappings = {
         'MiniLM': MiniLM,
         'MiniLMNoLN': MiniLMNoLN,
-        'MiniLMPostLN': MiniLMPostLN
+        'MiniLMPostLN': MiniLMPostLN,
+        'MiniLMNoPE': MiniLMNoPE
     }
     if model not in mappings:
         raise NotImplementedError
